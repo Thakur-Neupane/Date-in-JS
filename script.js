79ab928dd96bec8563f07f4a9ffc88e488be6b73
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-04-10 23:48:23 +1000

    Date in JS

---
+++ script.js
@@ -0,0 +1,37 @@
+// const today = new Date();
+// const data = today.getDate();
+// const data = today.getDay();
+// const data = today.getFullYear();
+// const data = today.getFullYear();
+// const data = today.getMonth();
+// const data = today.toLocaleDateString();
+// const data = today.toISOString();
+// today.setDate(today.getDate() + 10);
+// const data = today.getTime();
+// const datee = new Date(1712708536744);
+
+
+
+// console.log(data);
+
+
+const checkExp = () => {
+    
+
+
+};
+const foods = [
+    {
+        name: "Tuna",
+        exp:"2024-3-25",
+    },
+    {
+        name: "Milk",
+        exp: "2024-4-15",
+
+    },
+]
+
+foods.map((food) => {
+    console.log(food, i);
+});
